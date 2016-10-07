aa8701f6d827ed2cc9f84a4d7278c102c578b35a
changyu <changyu@10.180.64.176>
2016-10-07 00:51:49 -0400

    Finished

---
+++ ps3/src/test/java/ps3/AccountTest.java
@@ -0,0 +1,49 @@
+package ps3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AccountTest {
+	static Account testAccount;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		testAccount= new Account(1122,20000);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		testAccount= null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		testAccount.setAnnualInterestRate(4.5);
+		
+		try{
+			testAccount.withdraw(2500);
+		} catch(InsufficientFundsException e){
+			System.out.println("You cannot withdraw.Your balance is below"+e.getAmount());
+		}
+		
+		testAccount.deposit(3000);
+		System.out.println("Your balance is:"+testAccount.getBalance()+
+				" The monthly interest is"+testAccount.getMonthlyInterestRate()+
+				" The date that the account was created was:"+
+				testAccount.getDateCreated());
+	}
+
+}
